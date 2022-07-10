7895c7e6bf12ba02df41ee4336d06d0080126617
drili <DriliB@hotmail.com>
2022-07-10 18:23:12 +0200

    :hammer: Init e#1

--- next.config.js
+++ next.config.js
@@ -1,7 +1,5 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
+module.exports = {
+    env: {
+        MONGO_URI: "mongodb+srv://dbkynetic:Kynetic123123@cluster0.vyjisag.mongodb.net/?retryWrites=true&w=majority"
+    }
 }
-
-module.exports = nextConfig
